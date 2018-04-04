a9ad5afb4389c3985d7f02f6036c5f5c33223edc
Alisson Oliveira <jose.valerio@dataprev.gov.br>
2018-04-04 18:49:43 -0300

    remove dependência javolution do commons

--- Commons/src/main/java/com/l2jbr/ConsoleLogFormatter.java
+++ Commons/src/main/java/com/l2jbr/ConsoleLogFormatter.java
@@ -18,8 +18,6 @@
  */
 package com.l2jbr;
 
-import javolution.text.TextBuilder;
-
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.logging.Formatter;
@@ -27,45 +25,37 @@ import java.util.logging.LogRecord;
 
 /**
  * This class ...
+ *
  * @version $Revision: 1.1.4.2 $ $Date: 2005/03/27 15:30:08 $
  */
 
-public class ConsoleLogFormatter extends Formatter
-{
-	
-	/*
-	 * (non-Javadoc)
-	 * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
-	 */
-	// private static final String _ = " ";
-	private static final String CRLF = "\r\n";
-	
-	@Override
-	public String format(LogRecord record)
-	{
-		TextBuilder output = new TextBuilder();
-		// output.append(record.getLevel().getName());
-		// output.append(_);
-		// output.append(record.getLoggerName());
-		// output.append(_);
-		output.append(record.getMessage());
-		output.append(CRLF);
-		if (record.getThrown() != null)
-		{
-			try
-			{
-				StringWriter sw = new StringWriter();
-				PrintWriter pw = new PrintWriter(sw);
-				record.getThrown().printStackTrace(pw);
-				pw.close();
-				output.append(sw.toString());
-				output.append(CRLF);
-			}
-			catch (Exception ex)
-			{
-			}
-		}
-		
-		return output.toString();
-	}
+public class ConsoleLogFormatter extends Formatter {
+
+    /*
+     * (non-Javadoc)
+     * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
+     */
+    // private static final String _ = " ";
+    private static final String CRLF = "\r\n";
+
+    @Override
+    public String format(LogRecord record) {
+        StringBuilder output = new StringBuilder();
+
+        output.append(record.getMessage());
+        output.append(CRLF);
+        if (record.getThrown() != null) {
+            try {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                record.getThrown().printStackTrace(pw);
+                pw.close();
+                output.append(sw.toString());
+                output.append(CRLF);
+            } catch (Exception ex) {
+            }
+        }
+
+        return output.toString();
+    }
 }
--- Commons/src/main/java/com/l2jbr/FileLogFormatter.java
+++ Commons/src/main/java/com/l2jbr/FileLogFormatter.java
@@ -18,24 +18,23 @@
  */
 package com.l2jbr;
 
-import javolution.text.TextBuilder;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
-public class FileLogFormatter extends Formatter
-{
-	private static final String CRLF = "\r\n";
-	private static final String TAB = "\t";
-	private final SimpleDateFormat dateFmt = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss,SSS");
-	
-	@Override
-	public String format(LogRecord record)
-	{
-		TextBuilder output = new TextBuilder();
-		
-		return output.append(dateFmt.format(new Date(record.getMillis()))).append(TAB).append(record.getLevel().getName()).append(TAB).append(record.getThreadID()).append(TAB).append(record.getLoggerName()).append(TAB).append(record.getMessage()).append(CRLF).toString();
-	}
+public class FileLogFormatter extends Formatter {
+    private static final String CRLF = "\r\n";
+    private static final String TAB = "\t";
+    private final SimpleDateFormat dateFmt = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss,SSS");
+
+    @Override
+    public String format(LogRecord record) {
+        StringBuilder output = new StringBuilder();
+
+        return output.append(dateFmt.format(new Date(record.getMillis()))).append(TAB)
+                .append(record.getLevel().getName()).append(TAB).append(record.getThreadID())
+                .append(TAB).append(record.getLoggerName()).append(TAB).append(record.getMessage())
+                .append(CRLF).toString();
+    }
 }
