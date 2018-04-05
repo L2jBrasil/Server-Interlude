310fdbd4f0c27d28fbfa60e03807ddd6d407d02d
Alisson Oliveira <jose.valerio@dataprev.gov.br>
2018-04-05 13:34:27 -0300

    atualiza dependência de módulos

--- Commons/src/main/com.l2jbr.commons/module-info.java
+++ Commons/src/main/com.l2jbr.commons/module-info.java
@@ -1,7 +1,16 @@
 module com.l2jbr.commons {
+
     requires java.logging;
     requires java.sql;
     requires c3p0;
+    requires java.naming;
+    requires java.desktop;
+
     exports  com.l2jbr.commons;
+    exports com.l2jbr.commons.util;
+    exports com.l2jbr.commons.xml;
+    exports com.l2jbr.commons.crypt;
+    exports com.l2jbr.commons.status;
+    exports com.l2jbr.commons.lib;
 
 }
--- Loginserver/src/main/com.l2jbr.loginserver/module-info.java
+++ Loginserver/src/main/com.l2jbr.loginserver/module-info.java
@@ -1,2 +1,6 @@
 module com.l2jbr.loginserver {
+    requires com.l2jbr.commons;
+    requires java.xml;
+    requires java.sql;
+    requires com.l2jbr.mmocore;
 }
