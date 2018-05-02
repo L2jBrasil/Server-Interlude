4c0e955c3495a82bf2fcb3192d74c71487d9aba5
Alisson Oliveira <joe.alisson@gmail.com>
2018-05-02 18:47:30 -0300

    Remove referências diretas à tabela itemsonground

--- Gameserver/src/main/com.l2jbr.gameserver/com/l2jbr/gameserver/instancemanager/ItemsOnGroundManager.java
+++ Gameserver/src/main/com.l2jbr.gameserver/com/l2jbr/gameserver/instancemanager/ItemsOnGroundManager.java
@@ -18,19 +18,18 @@
 package com.l2jbr.gameserver.instancemanager;
 
 import com.l2jbr.commons.Config;
-import com.l2jbr.commons.database.L2DatabaseFactory;
+import com.l2jbr.commons.database.DatabaseAccess;
 import com.l2jbr.gameserver.ItemsAutoDestroy;
 import com.l2jbr.gameserver.ThreadPoolManager;
 import com.l2jbr.gameserver.model.L2ItemInstance;
 import com.l2jbr.gameserver.model.L2Object;
 import com.l2jbr.gameserver.model.L2World;
+import com.l2jbr.gameserver.model.database.ItemsOnGround;
+import com.l2jbr.gameserver.model.database.repository.ItemsOnGroundRepository;
 import com.l2jbr.gameserver.templates.L2EtcItemType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -75,89 +74,50 @@ public class ItemsOnGroundManager {
             return;
         }
 
+        ItemsOnGroundRepository repository = DatabaseAccess.getRepository(ItemsOnGroundRepository.class);
         // if DestroyPlayerDroppedItem was previously false, items curently protected will be added to ItemsAutoDestroy
         if (Config.DESTROY_DROPPED_PLAYER_ITEM) {
-            java.sql.Connection con = null;
-            try {
-                String str = null;
-                if (!Config.DESTROY_EQUIPABLE_PLAYER_ITEM) {
-                    str = "update itemsonground set drop_time=? where drop_time=-1 and equipable=0";
-                } else if (Config.DESTROY_EQUIPABLE_PLAYER_ITEM) {
-                    str = "update itemsonground set drop_time=? where drop_time=-1";
-                }
-                con = L2DatabaseFactory.getInstance().getConnection();
-                PreparedStatement statement = con.prepareStatement(str);
-                statement.setLong(1, System.currentTimeMillis());
-                statement.execute();
-                statement.close();
-            } catch (Exception e) {
-                _log.error( "error while updating table ItemsOnGround " + e);
-                e.printStackTrace();
-            } finally {
-                try {
-                    con.close();
-                } catch (Exception e) {
-                }
+            if (!Config.DESTROY_EQUIPABLE_PLAYER_ITEM) {
+                repository.updateDropTimeNotEquipable(System.currentTimeMillis());
+            } else if (Config.DESTROY_EQUIPABLE_PLAYER_ITEM) {
+                repository.updateDropTime(System.currentTimeMillis());
             }
         }
 
-        // Add items to world
-        java.sql.Connection con = null;
-        try {
-            try {
-                con = L2DatabaseFactory.getInstance().getConnection();
-                Statement s = con.createStatement();
-                ResultSet result;
-                int count = 0;
-                result = s.executeQuery("select object_id,item_id,count,enchant_level,x,y,z,drop_time,equipable from itemsonground");
-                while (result.next()) {
-                    L2ItemInstance item = new L2ItemInstance(result.getInt(1), result.getInt(2));
-                    L2World.getInstance().storeObject(item);
-                    if (item.isStackable() && (result.getInt(3) > 1)) {
-                        item.setCount(result.getInt(3));
-                    }
-                    if (result.getInt(4) > 0) {
-                        item.setEnchantLevel(result.getInt(4));
-                    }
-                    item.getPosition().setWorldPosition(result.getInt(5), result.getInt(6), result.getInt(7));
-                    item.getPosition().setWorldRegion(L2World.getInstance().getRegion(item.getPosition().getWorldPosition()));
-                    item.getPosition().getWorldRegion().addVisibleObject(item);
-                    item.setDropTime(result.getLong(8));
-                    if (result.getLong(8) == -1) {
-                        item.setProtected(true);
-                    } else {
-                        item.setProtected(false);
-                    }
-                    item.setIsVisible(true);
-                    L2World.getInstance().addVisibleObject(item, item.getPosition().getWorldRegion(), null);
-                    _items.add(item);
-                    count++;
-                    // add to ItemsAutoDestroy only items not protected
-                    if (!Config.LIST_PROTECTED_ITEMS.contains(item.getItemId())) {
-                        if (result.getLong(8) > -1) {
-                            if (((Config.AUTODESTROY_ITEM_AFTER > 0) && (item.getItemType() != L2EtcItemType.HERB)) || ((Config.HERB_AUTO_DESTROY_TIME > 0) && (item.getItemType() == L2EtcItemType.HERB))) {
-                                ItemsAutoDestroy.getInstance().addItem(item);
-                            }
-                        }
+        repository.findAll().forEach(itemsOnGround -> {
+            L2ItemInstance item = new L2ItemInstance(itemsOnGround.getId(), itemsOnGround.getItemId());
+            L2World.getInstance().storeObject(item);
+            if (item.isStackable() && (itemsOnGround.getCount() > 1)) {
+                item.setCount(itemsOnGround.getCount());
+            }
+            if (itemsOnGround.getEnchantLevel() > 0) {
+                item.setEnchantLevel(itemsOnGround.getEnchantLevel());
+            }
+            item.getPosition().setWorldPosition(itemsOnGround.getX(), itemsOnGround.getY(), itemsOnGround.getZ());
+            item.getPosition().setWorldRegion(L2World.getInstance().getRegion(item.getPosition().getWorldPosition()));
+            item.getPosition().getWorldRegion().addVisibleObject(item);
+            item.setDropTime(itemsOnGround.getDropTime());
+            if (itemsOnGround.getDropTime() == -1) {
+                item.setProtected(true);
+            } else {
+                item.setProtected(false);
+            }
+            item.setIsVisible(true);
+            L2World.getInstance().addVisibleObject(item, item.getPosition().getWorldRegion(), null);
+            _items.add(item);
+            // add to ItemsAutoDestroy only items not protected
+            if (!Config.LIST_PROTECTED_ITEMS.contains(item.getItemId())) {
+                if (itemsOnGround.getDropTime() > -1) {
+                    if (((Config.AUTODESTROY_ITEM_AFTER > 0) && (item.getItemType() != L2EtcItemType.HERB)) ||
+                            ((Config.HERB_AUTO_DESTROY_TIME > 0) && (item.getItemType() == L2EtcItemType.HERB))) {
+                        ItemsAutoDestroy.getInstance().addItem(item);
                     }
                 }
-                result.close();
-                s.close();
-                if (count > 0) {
-                    System.out.println("ItemsOnGroundManager: restored " + count + " items.");
-                } else {
-                    System.out.println("Initializing ItemsOnGroundManager.");
-                }
-            } catch (Exception e) {
-                _log.error( "error while loading ItemsOnGround " + e);
-                e.printStackTrace();
             }
-        } finally {
-            try {
-                con.close();
-            } catch (Exception e) {
-            }
-        }
+        });
+
+        _log.info("ItemsOnGroundManager: restored {} items.", _items.size());
+
         if (Config.EMPTY_DROPPED_ITEM_TABLE_AFTER_LOAD) {
             emptyTable();
         }
@@ -186,21 +146,8 @@ public class ItemsOnGroundManager {
     }
 
     public void emptyTable() {
-        java.sql.Connection conn = null;
-        try {
-            conn = L2DatabaseFactory.getInstance().getConnection();
-            PreparedStatement del = conn.prepareStatement("delete from itemsonground");
-            del.execute();
-            del.close();
-        } catch (Exception e1) {
-            _log.error( "error while cleaning table ItemsOnGround " + e1);
-            e1.printStackTrace();
-        } finally {
-            try {
-                conn.close();
-            } catch (Exception e1) {
-            }
-        }
+        ItemsOnGroundRepository repository = DatabaseAccess.getRepository(ItemsOnGroundRepository.class);
+        repository.deleteAll();
     }
 
     protected class storeInDb extends Thread {
@@ -225,43 +172,12 @@ public class ItemsOnGroundManager {
                     continue; // Cursed Items not saved to ground, prevent double save
                 }
 
-                java.sql.Connection con = null;
-                try {
-                    con = L2DatabaseFactory.getInstance().getConnection();
-                    PreparedStatement statement = con.prepareStatement("insert into itemsonground(object_id,item_id,count,enchant_level,x,y,z,drop_time,equipable) values(?,?,?,?,?,?,?,?,?)");
-                    statement.setInt(1, item.getObjectId());
-                    statement.setInt(2, item.getItemId());
-                    statement.setInt(3, item.getCount());
-                    statement.setInt(4, item.getEnchantLevel());
-                    statement.setInt(5, item.getX());
-                    statement.setInt(6, item.getY());
-                    statement.setInt(7, item.getZ());
+                ItemsOnGround itemsOnGround = new ItemsOnGround(item);
+                ItemsOnGroundRepository repository = DatabaseAccess.getRepository(ItemsOnGroundRepository.class);
+                repository.save(itemsOnGround);
+            }
+            _log.debug("ItemsOnGroundManager: {} items on ground saved.", _items.size());
 
-                    if (item.isProtected()) {
-                        statement.setLong(8, -1); // item will be protected
-                    } else {
-                        statement.setLong(8, item.getDropTime()); // item will be added to ItemsAutoDestroy
-                    }
-                    if (item.isEquipable()) {
-                        statement.setLong(9, 1); // set equipable
-                    } else {
-                        statement.setLong(9, 0);
-                    }
-                    statement.execute();
-                    statement.close();
-                } catch (Exception e) {
-                    _log.error( "error while inserting into table ItemsOnGround " + e);
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        con.close();
-                    } catch (Exception e) {
-                    }
-                }
-            }
-            if (Config.DEBUG) {
-                _log.warn("ItemsOnGroundManager: " + _items.size() + " items on ground saved");
-            }
         }
     }
 }
--- Gameserver/src/main/com.l2jbr.gameserver/com/l2jbr/gameserver/model/database/ItemsOnGround.java
+++ Gameserver/src/main/com.l2jbr.gameserver/com/l2jbr/gameserver/model/database/ItemsOnGround.java
@@ -1,16 +1,75 @@
 package com.l2jbr.gameserver.model.database;
 
+import com.l2jbr.commons.database.annotation.Column;
+import com.l2jbr.commons.database.model.Entity;
+import com.l2jbr.gameserver.model.L2ItemInstance;
+import org.springframework.data.annotation.Id;
 
+public class ItemsOnGround extends Entity<Integer> {
 
-public class ItemsOnGround  {
-    private int object_id;
-    private int item_id;
+    @Id
+    @Column("object_id")
+    private int objectId;
+    @Column("item_id")
+    private int itemId;
     private int count;
-    private int enchant_level;
+    @Column("enchant_level")
+    private int enchantLevel;
     private int x;
     private int y;
     private int z;
-    private long drop_time;
+    @Column("drop_time")
+    private long dropTime;
     private int equipable;
 
+    public ItemsOnGround() {}
+
+    public ItemsOnGround(L2ItemInstance item) {
+        this.objectId = item.getObjectId();
+        this.itemId = item.getItemId();
+        this.count = item.getCount();
+        this.enchantLevel = item.getEnchantLevel();
+        this.x = item.getX();
+        this.y = item.getY();
+        this.z = item.getZ();
+        this.dropTime = item.isProtected() ? -1 : item.getDropTime();
+        this.equipable = item.isEquipable() ? 1 : 0;
+    }
+
+    @Override
+    public Integer getId() {
+        return objectId;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getEnchantLevel() {
+        return enchantLevel;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public long getDropTime() {
+        return dropTime;
+    }
+
+    public int getEquipable() {
+        return equipable;
+    }
 }
